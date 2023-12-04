e53692833d0befaa3be082aa614e291f5c9fa351
moon-123 <didansrl123@naver.com>
2023-12-05 08:29:01 +0900

    사이드바 수정

--- js/index.js
+++ js/index.js
@@ -16,18 +16,23 @@
 // })
 
 
-document.addEventListener("DOMContentLoaded", () => {
+
+window.onload = function(){
     const sidebar = document.querySelector(".side-bar");
     const plusButton = document.querySelector(".plus");
     const closeButton = document.querySelector(".close");
+    const main = document.querySelector(".main")
 
+    
     plusButton.addEventListener("click", (e) => {
         sidebar.classList.add('fadeIn');
         sidebar.classList.remove('fadeOut');
+        main.style.visibility = "hidden"
     });
 
     closeButton.addEventListener("click", (e) => {
         sidebar.classList.add('fadeOut');
         sidebar.classList.remove('fadeIn');
+        main.style.visibility = "visible"
     });
-});
+}
